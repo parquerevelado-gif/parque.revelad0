c8c26599213bcee3dd3db3e33e668963537c5d7e
Abril Beltramone <parquerevelado@gmail.com>
2025-08-04 16:57:33 -0300

    Actualizo JS para cargar imágenes desde Google Sheets y saco título amarillo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const urlCSV = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSSSez9h2cXtIeyHfzeUgHGkPYoM6CVPagA11PH39ZM4m7ieT5UwpXPuAKcM08OWYp1FDvSFKabR7na/pub?output=csv';
+
 $(document).ready(function () {
   $('.ripple-bg').ripples({
     resolution: 128,
@@ -5,30 +7,68 @@ $(document).ready(function () {
     dropRadius: 20,
     interactive: true
   });
-});
 
-const fotos = [
-  "https://picsum.photos/id/1015/200/150",
-  "https://picsum.photos/id/1025/200/150",
-  "https://picsum.photos/id/1035/200/150",
-  "https://picsum.photos/id/1050/200/150",
-  "https://picsum.photos/id/1060/200/150"
-];
+  let fotos = [];
+  let fotosDisponibles = [];
 
-document.querySelector(".ripple-bg").addEventListener("click", function (e) {
-  const contenedor = document.getElementById("recuerdos-container");
-  const img = document.createElement("img");
-  img.classList.add("recuerdo-img");
-  const random = fotos[Math.floor(Math.random() * fotos.length)];
-  img.src = random;
-  const x = e.clientX;
-  const y = e.clientY;
-  img.style.left = `${x - 100}px`;
-  img.style.top = `${y - 75}px`;
-  contenedor.appendChild(img);
-});
+  function limpiarLista(lista) {
+    const set = new Set();
+    lista.forEach(item => {
+      const trimmed = item.trim();
+      if(trimmed) set.add(trimmed);
+    });
+    return Array.from(set);
+  }
 
-document.getElementById("borrar-recuerdos").addEventListener("click", function () {
-  const contenedor = document.getElementById("recuerdos-container");
-  contenedor.innerHTML = "";
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function resetFotosDisponibles() {
+    fotosDisponibles = [...fotos];
+    shuffleArray(fotosDisponibles);
+    console.log('Fotos disponibles reseteadas, total:', fotosDisponibles.length);
+  }
+
+  fetch(urlCSV)
+    .then(res => res.text())
+    .then(csvText => {
+      fotos = limpiarLista(csvText.split('\n'));
+      resetFotosDisponibles();
+
+      const $contenedor = $("#recuerdos-container");
+      const $fondo = $(".ripple-bg");
+
+      $fondo.on("click", function (e) {
+        if(fotosDisponibles.length === 0) {
+          resetFotosDisponibles();
+          console.log('Lista vacía, reseteando...');
+        }
+
+        const imgSrc = fotosDisponibles.shift();
+        console.log('Mostrando imagen:', imgSrc, 'quedan', fotosDisponibles.length);
+
+        const img = $("<img>")
+          .addClass("recuerdo-img")
+          .attr("src", imgSrc)
+          .css({
+            left: e.clientX - 100 + "px",
+            top: e.clientY - 75 + "px",
+            position: "absolute",
+            pointerEvents: "none",
+            zIndex: 2
+          });
+        $contenedor.append(img);
+      });
+
+      $("#borrar-recuerdos").on("click", function () {
+        $contenedor.empty();
+      });
+    })
+    .catch(err => {
+      console.error("Error al cargar las fotos:", err);
+    });
 });
